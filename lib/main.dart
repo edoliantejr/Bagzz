a07385fffbdce90ac5c5910ea96f3995c841c772
edoliantesjr <edoliantesjr@gmail.com>
2021-08-06 00:38:24 +0800

    Implement bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -20,137 +20,143 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int numCartItems = 0;
+
   @override
   Widget build(BuildContext context) {
     num devicePixel = MediaQuery.of(context).devicePixelRatio;
     final PageController controller =
         PageController(initialPage: 0, keepPage: true);
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          leading: Container(
-              margin: EdgeInsets.symmetric(
-                  horizontal: 24 / devicePixel, vertical: 14 / devicePixel),
-              child: drawerIcon),
-          title: const Text(
-            'bagzz',
-            style: TextStyle(
-                color: Colors.black,
-                fontFamily: 'Playfair',
-                fontSize: 26,
-                fontWeight: FontWeight.bold),
-          ),
-          actions: const [
-            Padding(
-                padding: EdgeInsets.all(10),
-                child: CircleAvatar(
-                  backgroundColor: Colors.black,
-                  backgroundImage: AssetImage('assets/avatar.jpg'),
-                  radius: 18,
-                ))
-          ],
+        elevation: 0,
+        leading: Container(
+            margin: EdgeInsets.symmetric(
+                horizontal: 24 / devicePixel, vertical: 14 / devicePixel),
+            child: drawerIcon),
+        title: const Text(
+          'bagzz',
+          style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Playfair',
+              fontSize: 26,
+              fontWeight: FontWeight.bold),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 28, horizontal: 12),
-              height: 205,
-              width: 714,
-              child: Stack(
-                fit: StackFit.loose,
-                children: [
-                  PageView.builder(
-                      controller: controller,
-                      itemCount: bags.length,
-                     
-                      itemBuilder: (context, i) {
-                        return imageSlide(bags[i].image, bags[i].title);
-                      }),
-                  Positioned(
-                    top: 154,
-                    right: 10,
-                    child: Row(children: [
-                      Container(
-                        height: 51,
-                        color: Colors.black,
-                        width: 51,
-                        child: IconButton(
-                          color: Colors.white,
-                          onPressed: () {
-        
-                            controller.previousPage(
-                                duration: Duration(milliseconds: 300),
-                                curve: Curves.easeInOut);
-                          },
-                          icon: Icon(Icons.arrow_back),
-                          iconSize: 26,
-                        ),
+        actions: const [
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: CircleAvatar(
+                backgroundColor: Colors.black,
+                backgroundImage: AssetImage('assets/avatar.jpg'),
+                radius: 18,
+              ))
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 28, horizontal: 12),
+            height: 205,
+            width: 714,
+            child: Stack(
+              fit: StackFit.loose,
+              children: [
+                PageView.builder(
+                    controller: controller,
+                    itemCount: bags.length,
+                    itemBuilder: (context, i) {
+                      return imageSlide(bags[i].image, bags[i].title);
+                    }),
+                Positioned(
+                  top: 154,
+                  right: 10,
+                  child: Row(children: [
+                    Container(
+                      height: 51,
+                      color: Colors.black,
+                      width: 51,
+                      child: IconButton(
+                        color: Colors.white,
+                        onPressed: () {
+                          controller.previousPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.easeInOut);
+                        },
+                        icon: Icon(Icons.arrow_back),
+                        iconSize: 26,
                       ),
-                      SizedBox(
-                        width: 1,
+                    ),
+                    SizedBox(
+                      width: 1,
+                    ),
+                    Container(
+                      height: 51,
+                      color: Colors.black,
+                      width: 51,
+                      child: IconButton(
+                        color: Colors.white,
+                        onPressed: () {
+                          controller.nextPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.easeInOut);
+                        },
+                        icon: Icon(Icons.arrow_forward),
+                        iconSize: 26,
                       ),
-                      Container(
-                        height: 51,
-                        color: Colors.black,
-                        width: 51,
-                        child: IconButton(
-                          color: Colors.white,
-                          onPressed: () {
-                           controller.nextPage(
-                                duration: Duration(milliseconds: 300),
-                                curve: Curves.easeInOut);
-                          },
-                          icon: Icon(Icons.arrow_forward),
-                          iconSize: 26,
-                        ),
-                      )
-                    ]),
-                  )
-                ],
-              ),
+                    )
+                  ]),
+                )
+              ],
             ),
-          ],
-        ),
-        bottomNavigationBar: DotNavigationBar(
-          currentIndex: -1,
-          onTap: (int index) {},
-          items: [
-            DotNavigationBarItem(
-              icon: const Icon(
-                CustomIcon.home,
-              ),
-            ),
-            DotNavigationBarItem(
-              icon: const Icon(
-                CustomIcon.search,
-              ),
-            ),
-            DotNavigationBarItem(
-              icon: const Icon(
-                CustomIcon.favorite,
-              ),
-            ),
-            DotNavigationBarItem(
-              icon: const Icon(
-                CustomIcon.shoppping_bag,
-              ),
-            ),
-          ],
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 5,
-            )
-          ],
-          itemPadding: EdgeInsets.symmetric(
-            vertical: 20 / devicePixel,
-            horizontal: 18 / devicePixel,
           ),
-          paddingR: const EdgeInsets.all(14),
-          marginR: const EdgeInsets.all(8),
-        ));
+        ],
+      ),
+      bottomNavigationBar: Stack(
+        children: [
+          DotNavigationBar(
+            currentIndex: -1,
+            onTap: (int index) {},
+            items: [
+              DotNavigationBarItem(
+                icon: const Icon(
+                  CustomIcon.home,
+                ),
+              ),
+              DotNavigationBarItem(
+                icon: const Icon(
+                  CustomIcon.search,
+                ),
+              ),
+              DotNavigationBarItem(
+                icon: const Icon(
+                  CustomIcon.favorite,
+                ),
+              ),
+              DotNavigationBarItem(
+                icon: const Icon(
+                  CustomIcon.shoppping_bag,
+                ),
+              ),
+            ],
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 5,
+              )
+            ],
+            itemPadding: EdgeInsets.symmetric(
+              vertical: 23,
+              horizontal: 29,
+            ),
+            //paddingR: const EdgeInsets.all(14),
+            marginR: const EdgeInsets.all(8),
+          ),
+          numCartItem(2),
+        ],
+      ),
+    );
   }
 }
 
@@ -187,5 +193,33 @@ Widget imageSlide(String? image, String? title) {
         ),
       ),
     ],
+  );
+}
+
+Widget numCartItem(int numCartItems) {
+  Color color = Colors.black;
+  return Positioned(
+    top: 33,
+    right: 28,
+    child: Container(
+      decoration: BoxDecoration(
+          color: color = numCartItems > 0 ? color : Colors.transparent,
+          border: Border.all(
+              color: color =
+                  numCartItems > 0 ? Colors.white : Colors.transparent),
+          borderRadius: BorderRadius.circular(40)),
+      height: 20,
+      width: 20,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(1.0),
+          child: Text(
+            '$numCartItems',
+            style: TextStyle(
+                color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    ),
   );
 }

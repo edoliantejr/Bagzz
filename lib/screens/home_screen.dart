f47d984f9db71b465f73fa6d6739ec29b8990056
edoliantesjr <edoliantesjr@gmail.com>
2021-08-10 16:23:37 +0800

    added bouncing physics in single child scrollview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-    physics: BouncingScrollPhysics(),
+      physics: BouncingScrollPhysics(),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
